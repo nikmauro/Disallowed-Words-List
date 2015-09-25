f2a12905a25e190dfcbe5fb067633ec4f8f8f9de
nikmauro <nikmauro@gmail.com>
2015-09-25 17:05:14 +0300

    Update dissalowed.php

--- dissalowed.php
+++ dissalowed.php
@@ -1,8 +1,17 @@
 <?php
 echo "hello world";
 
+function dissalowed_ConvertListToArray($list=null){
+  return $list;
+}
+
+function dissalowed_FecthList($list=null){
+  return $list;
+}
+
 function dissalowed_UserNameList(){
-  
+  $data=array();
+  return $data;
 }
 
 ?>
