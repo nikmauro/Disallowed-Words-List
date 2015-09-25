7d1def3bedbb9b87af1e43f700daa181a7865813
nikmauro <nikmauro@gmail.com>
2015-09-25 17:28:52 +0300

    Update dissalowed.php

--- dissalowed.php
+++ dissalowed.php
@@ -1,17 +1,36 @@
 <?php
-echo "hello world";
 
-function dissalowed_ConvertListToArray($list=null){
-  return $list;
+function dissalowed_ConvertListToArray($lines)
+{
+    $data = array();
+    // Loop through our array, show HTML source as HTML source; and line numbers too.
+    foreach ($lines as $line_num => $line) {
+        $data[] = $line;
+    }
+    return $data;
 }
 
-function dissalowed_FecthList($list=null){
-  return $list;
+function dissalowed_FecthList($list = null)
+{
+    // Get a file into an array.  In this example we'll go through HTTP to get
+// the HTML source of a URL.
+    $workingDir = dirname(__FILE__);
+    $file = $workingDir . DIRECTORY_SEPARATOR . $list;
+    $lines = file($file);
+    return $lines;
 }
 
-function dissalowed_UserNameList(){
-  $data=array();
-  return $data;
+function dissalowd_ReadList($list)
+{
+    $lines = dissalowed_FecthList($list);
+    return dissalowed_ConvertListToArray($lines);
+}
+
+function dissalowed_UserNameList()
+{
+    $listName = "DissalowedUserNamesList.txt";
+    $data = dissalowd_ReadList($listName);
+    return $data;
 }
 
 ?>
